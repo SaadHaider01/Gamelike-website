e9fc9692a6344fd0827f25f866d7b41879bda489
SaadHaider01 <saadhaider349@gmail.com>
2024-11-25 20:28:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import About from "./component/About.jsx"
+import Contact from "./component/Contact.jsx"
+import Features from "./component/Features.jsx"
+import Footer from "./component/Footer.jsx"
+import Hero from "./component/hero.jsx"
+import Navbar from "./component/Navbar.jsx"
+import Story from "./component/Story.jsx"
+
+
+function App() {
+  
+
+  return (
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
+
+export default App
